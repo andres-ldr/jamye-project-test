0d353e38e2f63fbf4e3658db4c0f107b95e48c06
Andres <lopez.andresrigoberto@gmail.com>
2024-07-02 20:23:09 -0700

    add events to socket io

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Server } from 'socket.io';
 
 dotenv.config();
 
@@ -12,7 +13,7 @@ const app = express();
 const server: http.Server = http.createServer(app);
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: String(process.env.FRONTEND_URL),
   credentials: true,
   optionsSuccessStatus: 200,
 };
@@ -20,6 +21,26 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const io = new Server(server, {
+  cors: {
+    origin: String(process.env.FRONTEND_URL),
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
+  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+  });
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
