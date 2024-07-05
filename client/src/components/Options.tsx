48c703e0fa344b7d95044d66e504eb6bf11c7450
Andres <lopez.andresrigoberto@gmail.com>
2024-07-04 20:42:17 -0700

    add form to add name and socket id

--- client/src/components/Options.tsx
+++ client/src/components/Options.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { SocketContext } from '@/context/SocketContext';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 interface OptionsProps {
   children: React.ReactNode;
@@ -16,12 +16,47 @@ const Options = ({ children }: OptionsProps) => {
     callAccepted,
     callEnded,
     me,
+    setName,
+    leaveCall,
+    callUser,
   } = useContext(SocketContext);
-  const [idToCall, setIdToCall] = useState('');
+  const [idToCall, setIdToCall] = useState(me || '');
 
   return (
-    <div>
-      <span>{me}</span>
+    <div className='flex flex-col gap-10'>
+      <form className='flex flex-col gap-4 py-2 px-4 border'>
+        <label>Name</label>
+        <input
+          type='text'
+          value={name}
+          className='py-1 px-2 border rounded-md'
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Call To ID</label>
+        <input
+          type='text'
+          value={idToCall}
+          onChange={(e) => setIdToCall(e.target.value)}
+          className='py-1 px-2 border rounded-md'
+        />
+        {callAccepted && !callEnded ? (
+          <button
+            type='button'
+            onClick={leaveCall}
+            className='py-1 px-2 bg-blue-500 rounded-md'
+          >
+            Hang Up
+          </button>
+        ) : (
+          <button
+            type='button'
+            onClick={() => callUser(idToCall)}
+            className='py-1 px-2 bg-blue-500 rounded-md'
+          >
+            Call
+          </button>
+        )}
+      </form>
       {children}
     </div>
   );
