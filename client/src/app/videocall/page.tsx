43c32e9c341eea19806afb4cb67ca55441888e04
Andres <lopez.andresrigoberto@gmail.com>
2024-07-04 20:41:24 -0700

    add socket provider

--- client/src/app/videocall/page.tsx
+++ client/src/app/videocall/page.tsx
@@ -1,40 +1,20 @@
-'use client';
 import Notifications from '@/components/Notifications';
 import Options from '@/components/Options';
 import VideoPlayer from '@/components/VideoPlayer';
 import { SocketProvider } from '@/context/SocketContext';
-import React, { useEffect } from 'react';
-import { io, Socket } from 'socket.io-client';
-
-const socket = io('http://localhost:4000');
+import React, { useEffect, useState } from 'react';
 
 const VideoCallPage = () => {
-  useEffect(() => {
-    const getSocketId = (id: string) => {
-      console.log(id);
-    };
-
-    socket.on('me', getSocketId);
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  return <div>video call page</div>;
-};
-
-export default VideoCallPage;
-
-/**
- *     <SocketProvider>
-       <div>
+  return (
+    <SocketProvider>
+      <div className='flex flex-col gap-4'>
         <VideoPlayer />
         <Options>
           <Notifications />
         </Options>
-      </div> 
-      <div></div>
+      </div>
     </SocketProvider>
- * 
- */
+  );
+};
+
+export default VideoCallPage;
