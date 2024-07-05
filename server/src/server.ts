d73db6079c338058a48770d1b90dbbd5a31645d6
Andres <lopez.andresrigoberto@gmail.com>
2024-07-04 17:55:31 -0700

    work in progress: websocket

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,9 +13,9 @@ const app = express();
 const server: http.Server = http.createServer(app);
 
 const corsOptions = {
-  origin: String(process.env.FRONTEND_URL),
+  origin: 'http://localhost:3000',
   credentials: true,
-  optionsSuccessStatus: 200,
+  // optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -23,20 +23,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const io = new Server(server, {
-  cors: {
-    origin: String(process.env.FRONTEND_URL),
-    methods: ['GET', 'POST'],
-  },
+  cors: corsOptions,
 });
 
 io.on('connection', (socket) => {
+  console.log(`user ${socket.id}`);
+
   socket.emit('me', socket.id);
+
   socket.on('disconnect', () => {
     socket.broadcast.emit('callEnded');
   });
+
   socket.on('callUser', ({ userToCall, signalData, from, name }) => {
     io.to(userToCall).emit('callUser', { signal: signalData, from, name });
   });
+
   socket.on('answerCall', (data) => {
     io.to(data.to).emit('callAccepted', data.signal);
   });
