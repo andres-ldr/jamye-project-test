bf3140c8dd1149a0cffda4e4f96abd164f91d8db
Andres <lopez.andresrigoberto@gmail.com>
2024-07-04 20:41:57 -0700

    add call state's components

--- client/src/components/Notifications.tsx
+++ client/src/components/Notifications.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client';
+import { SocketContext } from '@/context/SocketContext';
+import React, { useContext } from 'react';
 
 const Notifications = () => {
+  const { answerCall, call, callAccepted } = useContext(SocketContext);
   return (
-    <div>Notifications</div>
-  )
-}
+    <>
+      {call.isReceivingCall && !callAccepted && (
+        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <h1>{call.name} is calling:</h1>
+          <button
+            className='py-1 px-2 bg-blue-500 rounded-md'
+            onClick={answerCall}
+          >
+            Answer
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Notifications
+export default Notifications;
